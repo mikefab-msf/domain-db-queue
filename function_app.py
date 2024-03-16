9d8ae801c86397b126b66383b5f970d7864e4944
mikefab-msf <117652131+mikefab-msf@users.noreply.github.com>
2024-03-16 23:53:36 +0100

    Go to town

---
+++ function_app.py
@@ -0,0 +1,10 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp()
+
+@app.queue_trigger(arg_name="azqueue", queue_name="domain-checks-queue",
+                               connection="domaindbchecks_STORAGE") 
+def queue_trigger(azqueue: func.QueueMessage):
+    logging.info('Python Queue trigger processed a message: %s',
+                azqueue.get_body().decode('utf-8'))
