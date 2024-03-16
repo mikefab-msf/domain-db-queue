89669f16583df69a03626ded9d34d35e54dadf28
mikefab-msf <117652131+mikefab-msf@users.noreply.github.com>
2024-03-17 00:14:20 +0100

    ggrr

--- function_app.py
+++ function_app.py
@@ -4,7 +4,7 @@ import logging
 app = func.FunctionApp()
 
 @app.queue_trigger(arg_name="azqueue", queue_name="domain-checks-queue",
-                               connection="domaindbchecks_STORAGE") 
+                               connection="AzureWebJobsStorage") 
 def queue_trigger(azqueue: func.QueueMessage):
     logging.info('Python Queue trigger processed a message: %s',
                 azqueue.get_body().decode('utf-8'))
